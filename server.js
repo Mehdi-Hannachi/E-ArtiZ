dd5d6a0aa547b29b621f2fc156ce9b014beca604
Firas <mehdihannachi02@gmail.com>
2021-12-20 16:47:51 +0100

    define user login route & authentification using passport jwt

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ require("dotenv").config({ path: "./config/.env" });
 
 const express = require("express");
 const connectDB = require("./config/connectDB");
+const multer = require("multer");
 const user = require("./routes/user");
 const app = express();
 
 //parse data
 
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 app.use("/user", user);
 
