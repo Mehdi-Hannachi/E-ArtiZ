41cdfd2eb400d17f7a21b8e41a14990eab8b88a9
Firas <mehdihannachi02@gmail.com>
2021-12-23 16:58:29 +0100

    define delete product route

--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { addProduct } = require("../controllers/product.controller");
+const {
+  addProduct,
+  deleteProduct,
+  getProductById,
+} = require("../controllers/product.controller");
 const isAuth = require("../middlewares/passport-setup");
 
 const Router = express.Router();
@@ -8,5 +12,15 @@ const Router = express.Router();
 // add product
 
 Router.post("/addProduct", isAuth(), addProduct);
+
+//  http://localhost:9000/product/deleteProduct/:idUser/:idProduct
+// delete Product
+
+Router.delete("/deleteProduct/:idUser/:idProduct", deleteProduct);
+
+//  http://localhost:9000/product/getProduct/:id
+// get Product by id
+
+Router.get("/getProduct/:id", getProductById);
 
 module.exports = Router;
